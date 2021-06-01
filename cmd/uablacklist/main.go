2c934b8e023108345346dbf5059cff3cdcea5dd2
root <root@vesta-amd64.hl.ua>
2021-06-01 18:30:30 +0300

    First work prototype

--- cmd/uablacklist/main.go
+++ cmd/uablacklist/main.go
@@ -2,17 +2,27 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/RomanAvdeenko/uablacklist/internal/app/uablacklist"
 )
 
-var configPath string
+var (
+	jsonURL,
+	resultFileName string
+)
 
 func init() {
-	flag.StringVar(&configPath, "url", "https://uablacklist.net/all.json", "JSON URL address")
+	flag.StringVar(&jsonURL, "url", "https://uablacklist.net/all.json", "JSON URL address")
+	flag.StringVar(&resultFileName, "out", "out.txt", "result file name")
 	flag.Parse()
 }
 func main() {
-	cfg := uablacklist.NewConfig(configPath)
-	uablacklist.Start(*cfg)
+	cfg := uablacklist.NewConfig(jsonURL, resultFileName)
+
+	if err := uablacklist.Start(*cfg); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/RomanAvdeenko/uablacklist
 
 go 1.16
+
+require (
+	go.uber.org/zap v1.17.0
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5
+)
